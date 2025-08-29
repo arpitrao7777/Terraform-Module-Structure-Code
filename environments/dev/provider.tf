3509cb18b410e74165d42bb9e4dc12bbafb77e83
Arpit <arpit32971@gmail.com>
2025-08-29 14:40:20 +0530

    pipeline added

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -9,6 +9,12 @@ terraform {
       version = "3.7.2"
     }
   }
+  backend "azurerm" {
+     resource_group_name  = "rg-001"
+     storage_account_name = "test32971"
+     container_name       = "developement"
+     key                  = "main.terraform.tfstate"
+     }
 }
 
 provider "azurerm" {
